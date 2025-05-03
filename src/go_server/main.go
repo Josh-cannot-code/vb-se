e56e86e9a53ebe88dcfced3e90fe2f2101b1b077
josh-cannot-code <j.dallacqua1@gmail.com>
2025-05-03 18:58:48 -0400

    another refactor

--- src/go_server/main.go
+++ src/go_server/main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"go_server/components"
 	database "go_server/database/opensearch"
+	"go_server/rest"
 
-	"go_server/models"
 	"log/slog"
-	"net/http"
 	"os"
 
-	"github.com/a-h/templ"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -17,7 +14,6 @@ import (
 )
 
 func main() {
-
 	// Load environment variables
 	err := godotenv.Load("../.env")
 	if err != nil {
@@ -43,31 +39,10 @@ func main() {
 	log.Info("OpenSearch connection established")
 
 	// Routes
+	e.GET("/", rest.HandleSearch(db))
 	e.Static("/static", "./static")
 	e.File("/favicon.ico", "./static/favicon.ico")
 
-	e.GET("/", func(c echo.Context) error {
-		query := c.QueryParam("search")
-
-		var videos []*models.Video
-		err := error(nil)
-		if query != "" {
-			videos, err = db.SearchVideos(query, "relevance")
-			if err != nil {
-				e.Logger.Error("failed to search videos: ", err.Error())
-				return echo.NewHTTPError(http.StatusInternalServerError, "Failed to search videos")
-			}
-		}
-
-		return render(c, http.StatusOK, components.Index(videos))
-	})
-
 	// Start server
 	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
-}
-
-func render(ctx echo.Context, statusCode int, t templ.Component) error {
-	ctx.Response().Writer.WriteHeader(statusCode)
-	ctx.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
-	return t.Render(ctx.Request().Context(), ctx.Response().Writer)
 }
---
+++ src/go_server/rest/handler.go
@@ -0,0 +1,36 @@
+package rest
+
+import (
+	"go_server/components"
+	database "go_server/database"
+
+	"go_server/models"
+	"net/http"
+
+	"github.com/a-h/templ"
+	"github.com/labstack/echo/v4"
+)
+
+func HandleSearch(db database.Datasource) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		query := c.QueryParam("search")
+
+		var videos []*models.Video
+		if query != "" {
+			var err error
+			videos, err = db.SearchVideos(query, "relevance")
+			if err != nil {
+				c.Logger().Error("failed to search videos: ", err.Error())
+				return echo.NewHTTPError(http.StatusInternalServerError, "Failed to search videos")
+			}
+		}
+
+		return render(c, http.StatusOK, components.Index(videos))
+	}
+}
+
+func render(ctx echo.Context, statusCode int, t templ.Component) error {
+	ctx.Response().Writer.WriteHeader(statusCode)
+	ctx.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
+	return t.Render(ctx.Request().Context(), ctx.Response().Writer)
+}
