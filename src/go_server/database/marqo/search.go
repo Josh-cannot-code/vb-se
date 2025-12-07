58e4bf487c853f4890f1468df68df9b09f19c8e9
josh-cannot-code <j.dallacqua1@gmail.com>
2025-12-07 13:45:47 -0500

    backoff marqo connection

--- src/go_server/database/marqo/search.go
+++ src/go_server/database/marqo/search.go
@@ -3,7 +3,10 @@ package database
 import (
 	"fmt"
 	"go_server/models"
+	"time"
 )
+
+const maxRetries = 10
 
 type MarqoAccess struct {
 	client *MarqoClient
@@ -13,6 +16,14 @@ func GetMarqoAccess(host string) (*MarqoAccess, error) {
 	client, err := getMarqoClient(host)
 	if err != nil {
 		return nil, err
+	}
+
+	retryCount := 0
+	sleepDuration := 2 * time.Second
+	for !client.Ping() && retryCount < maxRetries {
+		fmt.Println("Waiting for Marqo to be available...")
+		time.Sleep(sleepDuration)
+		sleepDuration *= 2
 	}
 
 	if !client.Ping() {
